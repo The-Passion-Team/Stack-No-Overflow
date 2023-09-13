1eda37668b537c5c858075c0ba7b846070cfa998
havanduoc <havanduoc.dev@gmail.com>
2023-09-13 11:17:10 +0700

    responsive page login

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,3 +1,4 @@
+import NoSiderLayout from "~/layouts/NoSiderLayout"
 import OnlyHeaderLayout from "~/layouts/OnlyHeaderLayout"
 import { Ask, Companies, Home, LogIn, Profile, Questions, SignUp, Tags, Users } from "~/pages"
 
@@ -25,7 +26,7 @@ export const routes: props = [
     { path: Path.SignUp, page: SignUp, layout: OnlyHeaderLayout },
     { path: Path.Profile, page: Profile },
     { path: Path.Questions, page: Questions },
-    { path: Path.Ask, page: Ask, layout: OnlyHeaderLayout },
+    { path: Path.Ask, page: Ask, layout: NoSiderLayout },
     { path: Path.Tags, page: Tags },
     { path: Path.Users, page: Users },
     { path: Path.Companies, page: Companies },
