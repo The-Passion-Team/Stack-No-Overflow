7671fd4a5f0efcfb4315ab337fa825568a92e0aa
havanduoc <havanduoc.dev@gmail.com>
2023-10-10 23:39:08 +0700

    backend - router CRUD for Role

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,7 @@ import { HandleError } from "../middlewares"
 import authRouter from "./auth"
 import postRouter from "./posts"
 import tagRouter from "./tags"
+import roleRouter from "./roles"
 
 const initRoutes = (app: any) => {
     const v1 = "/api/v1"
@@ -9,6 +10,7 @@ const initRoutes = (app: any) => {
     app.use(`${v1}/auth`, authRouter())
     app.use(`${v1}/post`, postRouter())
     app.use(`${v1}/tag`, tagRouter())
+    app.use(`${v1}/role`, roleRouter())
 
     return app.use(HandleError.notFound)
 }
