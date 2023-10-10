0927f393799ab0f2356fc43fd3b3f1b122ec9a2e
havanduoc <havanduoc.dev@gmail.com>
2023-10-10 16:50:36 +0700

    backend - router CRUD for Post

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,14 @@
-import { notFound } from "./../middlewares/handleError"
+import { HandleError } from "../middlewares"
 import authRouter from "./auth"
+import postRouter from "./posts"
 
 const initRoutes = (app: any) => {
     const v1 = "/api/v1"
 
     app.use(`${v1}/auth`, authRouter())
+    app.use(`${v1}/post`, postRouter())
 
-    return app.use(notFound)
+    return app.use(HandleError.notFound)
 }
 
 export default initRoutes
