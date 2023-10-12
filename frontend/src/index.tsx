7f004a38e7e1c94e6b904b87fe04c091b8240e0b
havanduoc <havanduoc.dev@gmail.com>
2023-10-12 23:19:34 +0700

    edit persist store (redux)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,7 @@ import { ConfigProvider } from "antd"
 import { Provider } from "react-redux"
 import ReactDOM from "react-dom/client"
 import GlobalStyles from "./styles/GlobalStyles"
-import { StorePersistor, store } from "./redux/store"
+import { persistor, store } from "./redux/store"
 import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <PersistGate persistor={StorePersistor}>
+            <PersistGate persistor={persistor}>
                 <ConfigProvider theme={theme}>
                     <GlobalStyles>
                         <App />
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -6,40 +6,33 @@ import { persistStore, persistReducer } from "redux-persist"
 import storage from "redux-persist/lib/storage"
 import { useDispatch } from "react-redux"
 
-const persistConfig = {
+const rootPersistConfig = {
     key: "root",
     version: 1,
     storage,
-    whitelist: ["auth", "users", "formAsk"],
+    blacklist: ["formAsk"],
 }
 
-const combinedReducer = combineReducers({
-    auth: authReducer,
+const authPersistConfig = {
+    key: "auth",
+    version: 1,
+    storage,
+    whitelist: ["currentUser"],
+}
+
+const authPersistReducer = persistReducer(authPersistConfig, authReducer)
+
+const rootReducer = combineReducers({
+    auth: authPersistReducer,
     user: usersReducer,
     formAsk: formAskReducer,
 })
 
-const rootReducer = (state: any, action: any) => {
-    // if (action.type === "auth/logoutSuccess") {
-    // 	state = undefined;
-    // }
-    return combinedReducer(state, action)
-}
+const rootPersistReducer = persistReducer(rootPersistConfig, rootReducer)
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-export const store = configureStore({
-    reducer: persistedReducer,
-    // middleware: (getDefaultMiddleware) =>
-    // 	getDefaultMiddleware({
-    // 		serializableCheck: {
-    // 			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-    // 		}
-    // 	})
-})
-
-export const StorePersistor = persistStore(store)
+export const store = configureStore({ reducer: rootPersistReducer })
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
-
 export const useAppDispatch = () => useDispatch<AppDispatch>()
